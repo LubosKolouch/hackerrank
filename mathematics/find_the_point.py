44e84550b4d2dcd74d5d4dc0785c8c6589d6874f
Lubos Kolouch <lubos@kolouch.net>
2020-09-27 12:07:59 +0200

    Find the point

--- mathematics/find_the_point.py
+++ mathematics/find_the_point.py
@@ -1,6 +1,9 @@
+""" https://www.hackerrank.com/challenges/find-point/ """
+
+
 def findPoint(px, py, qx, qy):
     #
     # Write your code here.
     #
 
-    return 2*qx-px,2*qy-py
+    return 2*qx-px, 2*qy-py
